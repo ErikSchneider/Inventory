8ba1241c3108c7155d0d113ef55a119ddab0f1e6
Erik <eschneider1128@gmail.com>
2016-05-23 20:12:23 -0400

    initial push

---
+++ src/com/theironyard/Inventory.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+/**
+ * Created by Erik on 5/23/16.
+ */
+public class Inventory {
+    String item;
+    int quantity;
+
+    public Inventory(String item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+}
---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,64 @@
+package com.theironyard;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void createItem(Scanner scanner, ArrayList<Inventory> items) {
+        System.out.println("Enter the item you have acquired");
+        String text = scanner.nextLine();
+        Inventory item = new Inventory(text, -1);
+        items.add(item);
+    }
+
+    public static void removeItem(Scanner scanner, ArrayList<Inventory> items) {
+        System.out.println("Which item would you like to remove?");
+        String remove = scanner.nextLine();
+        try {
+            int selectNum = Integer.valueOf(remove);
+            items.remove(selectNum - 1);
+
+        }
+        catch (NumberFormatException e) {
+            System.out.println("You didn't type a number!");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Number isn't valid");
+        }
+    }
+
+    public static void updateQuantiity(Scanner scanner, ArrayList<Inventory> items) {
+        System.out.println("Which item would you like to adjust the quantity of?");
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Inventory> items = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Create new item");
+            System.out.println("2. Remove item");
+            System.out.println("3. Update quantity");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+
+                case ("1"):
+                    createItem(scanner, items);
+                    break;
+
+                case ("2"):
+                    removeItem(scanner, items);
+                    break;
+
+                case ("3"):
+                    updateQuantiity(scanner, items);
+                    break;
+
+            }
+        }
+    }
+}
